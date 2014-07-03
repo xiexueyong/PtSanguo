b1a8f4e7937d27acc04f0ff188212b58e45cb1f3
xiexueyong <xiexueyong@putaogame.com>
2014-07-03 19:46:42 +0800

    add BaseScene Class

--- source/com/putaolab/model/itemSpec/BuildingItemSpec.hx
+++ source/com/putaolab/model/itemSpec/BuildingItemSpec.hx
@@ -1,18 +1,22 @@
-package mainCity;
+package com.putaolab.module;
 /**
  * User: gaoyun
- * Date: 14-7-3
- * Time: 下午3:41
+ * Date: 14-7-1
+ * Time: 下午5:36
  * description：
  */
 
-class BuildingSpec {
+class BuildingItemSpec {
+
     public var id:String;
     public var X:Int;
     public var Y:Int;
     public var Class:String;
     public var buildName:String;
 
+
     public function new() {
     }
+
+
 }
